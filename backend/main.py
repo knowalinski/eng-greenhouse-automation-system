02700254124a020a9ea8f417988a05892f89ea4e
knowalinski <94801188+knowalinski@users.noreply.github.com>
2023-01-08 17:17:41 +0100

    dodane renderowanie html w flaskowym api dodane ścieżki do html-a i css-a

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "stary wstał"
+    return render_template('index.html')
+    
 
 
 @app.route("/data-collector", methods=['POST'])
